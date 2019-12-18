9cf078afd986964f754356ba932733af11b5ad72
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2019-12-18 18:08:04 +0100

    Create custom user model
    Create the custom user model. Create migrations file and also add the custom user model in settings.py file.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
