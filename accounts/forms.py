9abf8f12a746459c05dfe451623a6f73efc152a5
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2019-12-18 18:38:05 +0100

    Create user registration form

---
+++ accounts/forms.py
@@ -0,0 +1,70 @@
+from django.contrib.auth import get_user_model
+from django import forms
+
+
+User = get_user_model()
+
+
+class UserRegistrationForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Firstname'
+        })
+    )
+    last_name = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Lastname'
+        })
+    )
+    email = forms.EmailField(
+        label='',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+    )
+    username = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+    )
+    password = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password'
+        })
+    )
+    password_confirmation = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password')
+
+
+    def clean_password_confirmation(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password_confirmation']:
+            raise forms.ValidationError('Passwords don\t match')
+        return cd['password_confirmation']
+    
+
+    def save(self, commit=True):
+        instance = super().save(commit=commit)
+        instance.set_password(self.cleaned_data['password'])
+        if commit:
+            instance.save()
+        return instance
+        
