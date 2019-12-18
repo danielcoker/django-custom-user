717842a80b37ef3fea270267a3ee191ce41b7c20
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2019-12-18 19:32:36 +0100

    Add necessary login settings constants
    LOGIN_URL
    LOGIN_REDIRECT_URL
    LOGIN_REDIRECT_URL

--- myusr/settings.py
+++ myusr/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'accounts:dashboard'
+LOGOUT_REDIRECT_URL = 'accounts:login'
