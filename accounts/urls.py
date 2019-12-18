bfdd6a8964b057a18672e7367875d4d24c533525
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2019-12-18 18:39:34 +0100

    Add url pattern and view for user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import RegistrationView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('register/', RegistrationView.as_view(), name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import UserRegistrationForm
 
-# Create your views here.
+
+from django.views.generic.edit import FormView
+
+
+class RegistrationView(FormView):
+    template_name = 'accounts/register.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('accounts:register_success')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
